4e30d975757ef49ecbb66b956e4bc506a10f2b9a
anandteertha <raoanandteertha@gmail.com>
2025-10-11 17:35:12 -0400

    SE04-9 Create flask backend with a simple template

---
+++ src/backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Flask Backend!"
+
+@app.route('/api/data')
+def get_data():
+    data = {"message": "This is some data from your Flask API"}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True) # debug=True is for development, disable in production
