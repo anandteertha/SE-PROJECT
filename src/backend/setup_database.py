b5767d97efe744af40cbeeb49c0b5944171f9f77
anandteertha <raoanandteertha@gmail.com>
2025-10-18 16:03:24 -0400

    SE04-9 Create basic DDL queries

---
+++ src/backend/setup_database.py
@@ -0,0 +1,36 @@
+from mysql.connector import MySQLConnection
+from backend.queries.simple_queries import SimpleQueries
+
+
+class SetupDatabase:
+    def __init__(self, connection: MySQLConnection):
+        self.connection = connection
+        self.cursor = self.connection.cursor()
+        pass
+    
+    def create_user_authentication_table(self):
+        self.cursor.execute(SimpleQueries.CREATE_USER_AUTHENTICATION_TABLE.value)
+        print("CREATED USER TABLE..")
+        pass
+    
+    def create_menu_items_table(self):
+        self.cursor.execute(SimpleQueries.CREATE_MENU_ITEMS_TABLE.value)
+        print("CREATED MENU ITEMS TABLE..")
+        pass
+    
+    def create_user_cart_table(self):
+        self.cursor.execute(SimpleQueries.CREATE_USER_CART_TABLE.value)
+        print("CREATE USER CART TABLE..")
+        pass
+    
+    def create_user_menu_settings_table(self):
+        self.cursor.execute(SimpleQueries.CREATE_USER_MENU_SETTINGS.value)
+        print("CREATE USER MENU SETTINGS TABLE..")
+        pass
+    
+    def setup(self):
+        self.create_user_authentication_table()
+        self.create_menu_items_table()
+        self.create_user_cart_table()
+        self.create_user_menu_settings_table()
+        pass
