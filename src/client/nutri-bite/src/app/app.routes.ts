2027055d9a3d82aece91dbd7f4edddcdef3c4cc1
anandteertha <raoanandteertha@gmail.com>
2025-10-30 20:40:53 -0400

    SE04-11 Add basic routing and relative paths

--- src/client/nutri-bite/src/app/app.routes.ts
+++ src/client/nutri-bite/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Settings } from '@modules/settings/settings';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        title: "Customize your choices!",
+        component: Settings,
+        path: 'settings'
+    }
+];
