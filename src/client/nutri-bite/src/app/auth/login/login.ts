91cfd42bfacdb88b37b22bb118e1c7af1e4a4ce8
Advait2207 <59452681+Advait2207@users.noreply.github.com>
2025-11-06 01:24:34 -0500

    SE04-12: Redesign login and register pages with two-column UI

--- src/client/nutri-bite/src/app/auth/login/login.ts
+++ src/client/nutri-bite/src/app/auth/login/login.ts
@@ -1,68 +1,59 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
-import { ReactiveFormsModule } from '@angular/forms'; 
-import { RouterModule } from '@angular/router'; // For the "register" link
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms'; 
+import { RouterModule } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
-// Import all the Material modules you need
-import { MatCardModule } from '@angular/material/card';
+// Import all the Material modules
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  standalone: true, // Make it standalone
+  standalone: true,
   imports: [
     CommonModule,
     ReactiveFormsModule,
     RouterModule,
-    MatCardModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule
   ],
-  templateUrl: './login.html', // Use 'login.html'
-  styleUrls: ['./login.scss'] // Use 'login.scss'
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
 })
-
-export class Login { 
+export class Login {
 
   loginForm = new FormGroup({
-  loginId: new FormControl('', [Validators.required]), 
-  password: new FormControl('', [Validators.required])
+    loginId: new FormControl('', [Validators.required]), 
+    password: new FormControl('', [Validators.required])
   });
 
   constructor(private http: HttpClient, private router: Router) {}
 
   onSubmit() {
-    if (this.loginForm.valid) {
-      console.log('Login Form is valid. Sending to backend...');
-      
-      // 2. Use HttpClient to send the form value to Flask API
-      this.http.post<any>('http://127.0.0.1:5000/login', this.loginForm.value)
-        .subscribe({
-          next: (response) => {
-            console.log('Login Success!', response);
-
-            //save the token
-            localStorage.setItem('token', response.token);
-
-            //save the username
-            localStorage.setItem('username', response.username)
-
-            this.router.navigate(['/home']);
-
-          },
-          error: (error) => {
-            console.error('Login Error!', error);
-          }
-        });
-
-    } else {
+    if (!this.loginForm.valid) {
       console.log('Login Form is invalid');
+      return;
     }
+
+    console.log('Login Form is valid. Sending to backend...');
+
+    this.http.post<any>('http://127.0.0.1:5000/login', this.loginForm.value)
+      .subscribe({
+        next: (response) => {
+          console.log('Login Success!', response);
+
+          localStorage.setItem('token', response.token); 
+          localStorage.setItem('username', response.username);
+
+          this.router.navigate(['/home']);
+        },
+        error: (error) => {
+          console.error('Login Error!', error);
+        }
+      });
   }
 }
